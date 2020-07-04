fe8b4cf4a100a1af724af884e74377bea997ef0e
Kanhai Chhugani <kchhugani@streetlightsoftware.com>
2020-07-04 14:42:55 +0530

    adding feed.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+  
+  eleventyConfig.addPlugin(pluginRss);
 
   // You can return your Config object (optional).
   return {
